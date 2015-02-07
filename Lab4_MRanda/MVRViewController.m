279153cc0b99eedf32de990f1aa4b75de9f2b85f
ITCS2825 Kozlevcar <itcs2825_kozlevcar@lakeland-community-colleges-imac-5.local>
2015-02-06 20:05:17 -0500

    Lab 4 First Push

--- Lab4_MRanda/MVRViewController.m
+++ Lab4_MRanda/MVRViewController.m
@@ -9,7 +9,33 @@
 #import "MVRViewController.h"
 
 @interface MVRViewController ()
+//Outlets
+@property (weak, nonatomic) IBOutlet UITextField *AmountTextBox; //didn't need
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+@property (weak, nonatomic) IBOutlet UITextField *TipTextBox;
 
+//actions
+- (IBAction)tipSlider:(UISlider *)sender;
+- (IBAction)peopleStepper:(UIStepper *)sender;
+- (IBAction)calcButton:(UIButton *)sender;
+- (IBAction)AmountTextBox:(UITextField *)sender;
+- (IBAction)TipTextBox:(UITextField *)sender;
+
+//labels
+@property (weak, nonatomic) IBOutlet UILabel *peopleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *perPersonLabel;
+
+//numbers
+@property double amountDouble;
+@property double totalDouble;
+@property double  tipDouble;
+@property double peopleInteger;
+
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event;
 @end
 
 @implementation MVRViewController
@@ -26,4 +52,42 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tipSlider:(UISlider *)sender
+{
+    self.TipTextBox.text = [NSString stringWithFormat:@"%.2f", sender.value];
+    
+    // integers only
+    self.TipTextBox.text = [NSString stringWithFormat:@"%i", (int)sender.value];
+}
+
+
+- (IBAction)peopleStepper:(UIStepper *)sender
+{
+     self.peopleLabel.text = [NSString stringWithFormat:@"%i", (int)sender.value];
+}
+
+- (IBAction)calcButton:(UIButton *)sender
+{
+    
+    self.totalLabel.text = [NSString stringWithFormat:@"%f", _totalDouble];
+}
+
+- (IBAction)AmountTextBox:(UITextField *)sender
+{
+
+}
+
+- (IBAction)TipTextBox:(UITextField *)sender
+{
+    self.tipSlider.value = [self.TipTextBox.text floatValue];
+
+    
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    //keyboard go away
+    [self.AmountTextBox endEditing:YES];
+    [self.TipTextBox endEditing:YES];
+}
 @end
